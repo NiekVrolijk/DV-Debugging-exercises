b3ae8c648f76f1bb8528018353f02d722175771e
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-02-22 12:41:42 +0100

    ah

--- exercise5/index.js
+++ exercise5/index.js
@@ -1,25 +1,27 @@
-let launchReady = false;
-let fuelLevel = 17000;
+let crewReady = false;
+let fuelReady = false;
+
+let fuelLevel = 21000;
 let crewStatus = true;
 let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
-   launchReady = true;
+   fuelReady = true;
 } else {
    console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
+   fuelReady = false;
 }
 
-if (crewStatus && computerStatus === 'green'){
+if (crewStatus && computerStatus === 'green') {
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   crewReady = false;
 }
 
-if (launchReady) {
+if (fuelReady && crewReady) {
    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
    console.log('Liftoff!');
 } else {
